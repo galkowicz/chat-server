04a0c47efaa630242ccd533a0a3f454e2ff69c48
Orian <orian@creditstacks.com>
2020-06-17 15:06:38 +0300

    Add mysql2. Add sequelize. Add database util

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const path = require('path');
+const http = require('http')
 
-const express = require('express');
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+
+const server = http.createServer(app)
+
+const db = require('./util/database')
+
+server.listen(5000)
